037f65936a2889fa8dd481705c6ab0349dfc8f17
Luca Binotto <brising@libero.it>
2021-09-06 10:53:39 +0200

    Creazione Progetto

---
+++ TestEpicodeFinale/src/main/java/it/epicode/be/TestEpicodeFinaleApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEpicodeFinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEpicodeFinaleApplication.class, args);
+	}
+
+}
